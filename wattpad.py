1738083b4ea1356abc22683ca5a86f664c533246
nastyafilogina <74674457+nastyafilogina@users.noreply.github.com>
2020-12-04 04:42:29 +0300

    Update wattpad.py

--- wattpad.py
+++ wattpad.py
@@ -215,4 +215,4 @@ bot.polling(none_stop=True, interval=0)
 
 if __name__ == '__main__':
     server.debug = True
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 3000)))
