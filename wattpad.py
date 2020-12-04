ed3d6be747dcf5062d5390da3f8fe1f383b918fc
nastyafilogina <74674457+nastyafilogina@users.noreply.github.com>
2020-12-04 04:28:12 +0300

    Update wattpad.py

--- wattpad.py
+++ wattpad.py
@@ -17,12 +17,13 @@ def scroll(SCROLL_PAUSE_TIME, driver):
     time.sleep(SCROLL_PAUSE_TIME * 2)
 
 from selenium import webdriver
-import json
 import pypandoc
 import telebot
-import requests  
 import time
 import os
+from flask import Flask, request
+
+server = Flask(__name__)
 
 bot = telebot.TeleBot('1450326509:AAEaq_unPJX3BKSTx0QSjZGJMGzmg4oMEDc')
 
@@ -211,3 +212,7 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Я не понимаю. Напишите /help.")
 
 bot.polling(none_stop=True, interval=0)
+
+if __name__ == '__main__':
+    server.debug = True
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
